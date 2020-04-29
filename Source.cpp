518dbf7781237b52e4789733823b0606bbfa6e17
Javoris Brown <sasukeucheha@live.com>
2020-04-29 17:30:18 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,7 @@
 //  MySQLCPPConnection
 //
 //  Created by Zhimin Gao on 10/21/19.
-//  Copyright © 2019 Zhimin Gao. All rights reserved.
+//  Copyright Â© 2019 Zhimin Gao. All rights reserved.
 //
 //  Database Project - Javoris Brown S00333473
 //
@@ -142,86 +142,4 @@ void showMenu() {
     cout << "3 - Query #3" << endl;
     cout << "4 - Query #4" << endl;
     cout << "0 - Exit" << endl;
-}
-
-void HW() {
-    std::string instructorName;
-    int salary = 0;
-    bool nameFound = false;
-    int count = 0;
-
-    try {
-        // connect to sql server
-        Session sess("localhost", 33060, "infernokun", "RAINBOWsixSIEGE##11");
-
-        // use a database
-        sess.sql("use class1").execute();
-
-        // user input for instructor
-        cout << "Enter new instructor name: ";
-        cin >> instructorName;
-
-        // user input for salary
-        cout << "Enter desired salary for " << instructorName << ": ";
-        cin >> salary;
-
-        // execute needed sql command
-        RowResult myResult = sess.sql("select dept_name, avg(salary) from instructor group by dept_name").execute();
-        RowResult myResult2 = sess.sql("select dept_name, budget from department").execute();
-
-        int foundSalary = 0;
-        bool canHire = false;
-
-        // loop through the avg salary of each dept
-        for (Row row : myResult) {
-
-            int avgSal = 0;
-            int totalExpense = 0;
-
-            // convert avg salary to int
-            std::ostringstream oss1;
-            oss1 << row[1];
-            std::string val1 = oss1.str();
-            avgSal = std::stoi(val1);
-
-            // salary to the avg salary of each dept
-            totalExpense = salary + avgSal;
-
-            // loop through the budget of each dept
-            for (Row row2 : myResult2) {
-
-                int budget = 0;
-
-                // convert budget to int
-                std::ostringstream oss2;
-                oss2 << row2[1];
-                std::string val2 = oss2.str();
-                budget = std::stoi(val2);
-
-                // check if the total expense is less then or = to the budget
-                if (totalExpense <= budget) {
-                    canHire = true;
-
-                    cout << "The " << row2[0] << " department has a " << budget << " budget that can handle the " << salary << " salary!" << endl;
-                    cout << "The total will be: " << totalExpense << endl;
-                    cout << "Can hire = " << canHire << endl << endl;
-                    break;
-                }
-                else {
-                    // dept can't afford if it is above the budget
-
-                    canHire = false;
-
-                    cout << "The " << row2[0] << " department has a " << budget << " budget that cannot handle the " << salary << " salary!" << endl;
-                    cout << "The total will be: " << totalExpense << endl;
-                    cout << "Can hire = " << canHire << endl << endl;
-                    break;
-                }
-            }
-        }
-    }
-
-    catch (const mysqlx::Error & err) { cout << "ERROR: " << err << endl; }
-    catch (std::exception & ex) { cout << "STD EXCEPTION: " << ex.what() << endl; }
-    catch (const char* ex) { cout << "EXCEPTION: " << ex << endl; }
 }
